f372e26fbcf8c3958fdf5e0680684e9e277b96f5
mazax1ct <vova@giveandget.ru>
2020-06-10 13:37:00 +0300

    import landing to main site

--- build/js/main.js
+++ build/js/main.js
@@ -129,6 +129,125 @@ $(document).ready(function() {
     });
   }
 
+
+  /******js для страницы лендинга evolv******/
+  //аккордеон
+  $('.js-accordion-title').click(function() {
+    $(this).toggleClass('is-active');
+    $('.accordion__body[data-target=' + $(this).attr('data-link') + ']').slideToggle();
+    return false;
+  });
+
+  if($('body').width() < 768) {
+    //слайдер корпусов
+    if($('.js-list-1').length) {
+      $('.js-list-1').on('init', function(event, slick) {
+        var list = $('.js-list-1').find('.slick-dots');
+        $.each(list['0'].children, function(index, value) {
+          if(index < 9) {
+            list['0'].children[index].children['0'].innerText = '0' + list['0'].children[index].children['0'].innerText;
+          }
+        });
+      });
+
+      $('.js-list-1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true
+      });
+    }
+
+    //слайдер конфигураций
+    if($('.js-list-3').length) {
+      $('.js-list-3').on('init', function(event, slick) {
+        var list = $('.js-list-3').find('.slick-dots');
+        $.each(list['0'].children, function(index, value) {
+          if(index < 9) {
+            list['0'].children[index].children['0'].innerText = '0' + list['0'].children[index].children['0'].innerText;
+          }
+        });
+      });
+
+      $('.js-list-3').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true
+      });
+    }
+
+    //слайдер дизайна
+    if($('.js-list-5').length) {
+      $('.js-list-5').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#slider_arrow_left2"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#slider_arrow_right2"/></svg></button>',
+        mobileFirst: true
+      });
+    }
+
+    //слайдер конфигураций
+    if($('.js-list-8').length) {
+      $('.js-list-8').on('init', function(event, slick) {
+        var list = $('.js-list-8').find('.slick-dots');
+        $.each(list['0'].children, function(index, value) {
+          if(index < 9) {
+            list['0'].children[index].children['0'].innerText = '0' + list['0'].children[index].children['0'].innerText;
+          }
+        });
+      });
+
+      $('.js-list-8').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true
+      });
+    }
+  }
+
+  //слайдер игр
+  if($('.js-list-2').length) {
+    $('.js-list-2').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: true,
+      prevArrow: '<button class="slick-arrow slick-prev" aria-label="Назад" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#slider_arrow_left2"/></svg></button>',
+      nextArrow: '<button class="slick-arrow slick-next" aria-label="Вперед" type="button"><svg class="slick-arrow__arrow" aria-hidden="true"><use xlink:href="#slider_arrow_right2"/></svg></button>',
+      mobileFirst: true,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+          }
+        },
+      ]
+    });
+  }
+
+  //переключение игр в списке
+  $(document).on('click', '.js-game', function () {
+    $('.js-game').removeClass('is-active');
+    $(this).addClass('is-active');
+    return false;
+  });
+  /******js для страницы лендинга evolv******/
 });
 
 //перезапуск функции навешивания класса на шапку при скролле и ресайзе
