de0277c4af6233d19246d29759fd79edf6d8763c
mazax1ct <vova@giveandget.ru>
2020-07-20 19:34:25 +0300

    remake block-7

--- build/js/main.js
+++ build/js/main.js
@@ -371,19 +371,18 @@ $(document).on('click', '.js-popup-close', function () {
 //переключение табов стандарт/vip
 $(document).on('click', '.js-tab-changer', function () {
   var el = $(this);
-  if(el.hasClass('button--transparent')){
-    $('.js-tab-changer').addClass('button--transparent');
-    el.removeClass('button--transparent');
+  $('.js-tab-changer').removeClass('is-active');
+  el.addClass('is-active');
 
-    $('.configs-tab.is-active').animate({
-      opacity: 0
-    }, 400, function() {
-      $('.configs-tab.is-active').removeClass('is-active');
-      $('.configs-tab[data-target='+el.attr('data-target')+']').addClass('is-active').animate({
-        opacity: 1
-      }, 400);
-    });
-  }
+  $('.configs-tab.is-active').animate({
+    opacity: 0
+  }, 400, function() {
+    $('.configs-tab.is-active').removeClass('is-active');
+    $('.configs-tab[data-target='+el.attr('data-target')+']').addClass('is-active').animate({
+      opacity: 1
+    }, 400);
+  });
+
   return false;
 });
 
