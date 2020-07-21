e9dc275ba352557ddb949b10e9a49715a919c869
mazax1ct <vova@giveandget.ru>
2020-07-21 17:47:26 +0300

    new blocks

--- build/js/main.js
+++ build/js/main.js
@@ -319,8 +319,43 @@ $(document).ready(function() {
         ]
       });
     }
+
+    //слайдер сравнения конфигураций
+    if($('.js-list-12').length) {
+      $('.js-list-12').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        mobileFirst: true,
+        responsive: [
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 991,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          }
+        ]
+      });
+    }
   }
 
+});
+
+//переключение слайдера сравнения конфигураций
+$(document).on('click', '.js-compare-slide', function () {
+  $('.js-compare-slide').removeClass('is-active');
+  $(this).addClass('is-active');
+  $('.js-list-12').slick('slickGoTo', $(this).attr('data-href'));
+  return false;
 });
 
 //перезапуск функции навешивания класса на шапку при скролле и ресайзе
